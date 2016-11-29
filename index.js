d64b7b75e1190352679d476de3226c1271e06209
kylem038 <misenckj@gmail.com>
2016-11-29 15:01:10 -0700

    Working on moving away from webpack

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const shortid = require('shortid');
+const fs = require('fs');
 
+app.use(express.static('lib'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Tinyify';
 app.locals.urls = {};
+
+app.get('/', (request, response) => {
+  fs.readFile(`${__dirname}/index.html`, (err, file) => {
+    response.send(file);
+  });
+});
 
 app.get('/:id', (request, response) => {
   const { id } = request.params;
