fade1217b4eb2639842c6f62675467390cf77194
kylem038 <misenckj@gmail.com>
2016-11-30 15:35:46 -0700

    All gets are tested

--- test/test.js
+++ test/test.js
@@ -2,6 +2,15 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('../');
 
+describe('GET /', () => {
+
+  it('should return a 200 status code', (done) => {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
+
 describe('GET /urls', () => {
 
   beforeEach(() => {
@@ -25,5 +34,39 @@ describe('GET /urls', () => {
         urls: app.locals.db.urls.data
       }, done);
   });
+});
 
+describe('GET /:id', () => {
+  beforeEach(() => {
+    this.url = {"id":"HkrRLh2Ge","longUrl":"https://www.google.com/"};
+    app.locals.db.urls.data = [this.url];
+  });
+
+  afterEach(() => {
+    app.locals.db.urls.data = [];
+  });
+
+  it('should redirect when requesting with the provided id', (done) => {
+
+    request(app)
+      .get(`/${this.url.id}`)
+      .expect(301, done);
+  });
+});
+
+describe('GET /:id', () => {
+  beforeEach(() => {
+    this.url = {"id":"HkrRLh2Ge"};
+    app.locals.db.urls.data = [this.url];
+  });
+
+  afterEach(() => {
+    app.locals.db.urls.data = [];
+  });
+
+  it('should return a 404 if the longUrl is not valid', (done) => {
+    request(app)
+      .get(`/${this.url.id}`)
+        .expect(404, done);
+  });
 });
